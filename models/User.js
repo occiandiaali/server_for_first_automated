6a2eca0591306703b7f8391284d9e31450868e91
occiandiaali <occiandiaali@gmail.com>
2025-10-20 17:37:41 +0100

    Silenced role&auth middleware in adminRoutes inorder to get '/api/admin/staff'

--- models/User.js
+++ models/User.js
@@ -6,8 +6,8 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["user", "admin", "sales"], default: "user" }, // Role-based access
-  },
-  { collection: "staff" }
+  }
+  // { collection: "staff" }
 );
 
 module.exports = mongoose.model("User", userSchema);
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
+const User = require("../models/User");
 
 const router = express.Router();
 
@@ -9,14 +10,50 @@ router.get(
   authMiddleware,
   roleMiddleware("admin"),
   (req, res) => {
-    res.json({ message: "Welcome to Admin Dashboard", user: req.user });
+    res.json({ message: "Welcome to Dashboard", user: req.user });
   }
 );
-router.get("/staff", authMiddleware, roleMiddleware("admin"), (req, res) => {
-  res.json({ message: "Welcome to Admin Dashboard", user: req.user });
-});
+
+router.get(
+  "/staff",
+  // authMiddleware,
+  // roleMiddleware("admin"),
+  async (req, res) => {
+    try {
+      const staff = await User.find();
+      res.json(staff);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch staff" });
+    }
+    //res.json({ message: "Welcome to Staff Dashboard", user: req.user });
+    //   // Create a route to fetch todos
+    // app.get('/api/todos', async (req, res) => {
+    //   const todos = await Todo.find().sort({ createdAt: -1 });
+    //   res.json(todos)
+    // //client code
+    //     async fetchTodos() {
+    //       try {
+    //         const response = await axios.get('http://localhost:3000/api/todos');
+    //         this.todos = response.data;
+    //       } catch (error) {
+    //         console.error('Error fetching todos:', error);
+    //       }
+    //     }
+    // <ul>
+    //   <li v-for="todo in todos" :key="todo._id">{{ todo.title }}</li>
+    // </ul>
+  }
+);
 router.get("/items", authMiddleware, roleMiddleware("admin"), (req, res) => {
-  res.json({ message: "Welcome to Admin Dashboard", user: req.user });
+  res.json({ message: "Welcome to Items Dashboard", user: req.user });
 });
+router.get(
+  "/customers",
+  authMiddleware,
+  roleMiddleware("admin"),
+  (req, res) => {
+    res.json({ message: "Welcome to Customers Dashboard", user: req.user });
+  }
+);
 
 module.exports = router;
