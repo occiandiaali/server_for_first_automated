5f335fc4d4eb839e3a254b9de3de4c7cf7841133
occiandiaali <occiandiaali@gmail.com>
2025-10-22 16:13:30 +0100

    Add storefront post route;create Order model

---
+++ models/Order.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const garmentSchema = new mongoose.Schema({
+  //{name: 'Shirt(long)', price: 400, amt: 5, subTotal: 2000}
+  name: String,
+  price: Number,
+  amt: Number,
+  subTotal: Number,
+});
+
+const orderSchema = new mongoose.Schema({
+  customer: { type: String, required: true },
+  phone: { type: String, required: true },
+  comment: { type: String, default: "No comment" },
+  garments: { type: [garmentSchema], required: true },
+  dropOff: { type: String, required: true },
+  due: { type: String, required: true },
+  totalDue: { type: Number, required: true },
+  pickupPoint: { type: String, required: true },
+
+  // createdAt: { type: Date, default: Date.now }, // Sets current date as default
+});
+
+module.exports = mongoose.model("Order", orderSchema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Order = require("../models/Order");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
@@ -6,6 +7,8 @@ const router = express.Router();
 router.get("/profile", authMiddleware, (req, res) => {
   res.json({ message: "Welcome to User Profile", user: req.user });
 });
+
+router.post("/storefront", async (req, res) => {});
 router.get("/storefront", authMiddleware, (req, res) => {
   res.json({ message: "Welcome to Storefront", user: req.user });
 });
