54c617dd6be064b8dc9bc0f0ee7325f869fc9844
occiandiaali <occiandiaali@gmail.com>
2025-10-24 13:56:34 +0100

    Add Archive model to facilitate post & delete specific Order

---
+++ models/Archive.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const garmentSchema = new mongoose.Schema({
+  //{name: 'Shirt(long)', price: 400, amt: 5, subTotal: 2000}
+  name: String,
+  price: Number,
+  amt: Number,
+  subTotal: Number,
+});
+
+const archiveSchema = new mongoose.Schema({
+  orderNo: { type: String, required: true },
+  customer: { type: String, required: true },
+  phone: { type: String, required: true },
+  comment: { type: String, default: "No comment" },
+  garments: { type: [garmentSchema], required: true },
+  dropOff: { type: String, required: true },
+  due: { type: String, required: true },
+  totalDue: { type: Number, required: true },
+  pickupPoint: { type: String, required: true },
+
+  // createdAt: { type: Date, default: Date.now }, // Sets current date as default
+});
+
+module.exports = mongoose.model("Archive", archiveSchema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Archive = require("../models/Archive");
 const Order = require("../models/Order");
 const authMiddleware = require("../middleware/authMiddleware");
 
@@ -21,6 +22,38 @@ router.post("/storefront", async (req, res) => {
 router.get("/storefront", async (req, res) => {
   const orders = await Order.find();
   res.json(orders);
+});
+
+router.delete("/storefront/:id", async (req, res) => {
+  try {
+    const result = await Order.findByIdAndDelete(req.params.id);
+    if (!result) {
+      return res.status(404).send("Order not found");
+    }
+    res.send("Order now completed.");
+  } catch (error) {
+    res.status(500).send("Error deleting order");
+  }
+});
+
+router.post("/archive", async (req, res) => {
+  const { itemId } = req.body;
+
+  try {
+    const item = await Order.findById(itemId);
+    if (!item) return res.status(404).json({ message: "Order not found" });
+
+    // Optional: remove _id to avoid duplicate key error
+    const { _id, ...itemData } = item.toObject();
+
+    const newTargetItem = new Archive(itemData);
+    await newTargetItem.save();
+
+    res.status(200).json({ message: "Item archived successfully" });
+  } catch (error) {
+    console.error("Error archiving item:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 module.exports = router;
